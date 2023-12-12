51f23ec0cd168b50d6e656e6132f04213ba26417
visionthex <sandseclos@gmail.com>
2023-12-12 13:19:39 -0500

    updating files

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log('My code is running');
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+  if(inputBox.value === ''){
+    alert("You must write something in the text field!");
+  }
+  else{
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData(); // saves any of the created tasks.
+}
+
+listContainer.addEventListener("click", function(e){
+  if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked");
+    saveData(); // saves any checked items
+  } else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+    saveData(); // Saves any of the deleted data or/or checked items.
+  }
+}, false);
+
+function saveData(){ //Saves data on website
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
